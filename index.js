a219cb098eee9a5f7eb2069687372908d27ce2ef
Yann Thobena <thobena.yann@orange.fr>
2022-05-06 20:06:25 +0200

    error server handler

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const PORT = process.env.PORT ?? 4000;
 
 const server = new ApolloServer(app);
 
+process.on('uncaughtException', (error)  => {
+    console.log('Alert! ERROR : ',  error);
+    process.exit(1); // Exit your app 
+})
+
 server.listen(PORT).then(({ url }) => {
     debug(`🚀  Server is ready at ${url}
 📭  Query at https://studio.apollographql.com/dev`);
