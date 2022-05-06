4c992f4317b6512bde0038d241ffcff8b8fed940
Yann Thobena <thobena.yann@orange.fr>
2022-05-06 20:20:15 +0200

    log folder

--- app/helpers/logger.js
+++ app/helpers/logger.js
@@ -38,7 +38,7 @@ if (!['production', 'test'].includes(process.env.NODE_ENV)) {
 } else {
     streams.push({
         level: 'error', // On ne conserve que les logs a partir du niveau error
-        path: './log/error.log', // Le chemin du fichier de log (on pense à l'ignorer dans git)
+        path: '../log/error.log', // Le chemin du fichier de log (on pense à l'ignorer dans git)
         type: 'rotating-file', // on précise que l'on va faire une rotation de fichiers (Nouveau fichier dans une période défini + historique de fichiers conservés)
         period: '1d', // rotation des fichiers de log journalière
         count: 3, // On garde un historique de 3 fichiers de log (Donc ici 3 jours)
